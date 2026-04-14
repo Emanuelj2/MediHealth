1d9d96c4e4d4bd8591e3ef92b6e3052e2493c7de
Emanuel Jose <emanueljose0522@hotmail.com>
2026-04-13 20:43:03 -0400

    created the provider model

---
+++ Backend/Backend/Models/Provider.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    [Table("providers")]
+    public class Provider
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column("name")]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [Column("speciality")]
+        public string Speciality { get; set; } = string.Empty;
+        [Required]
+        [Column("office_address")]
+        public string Office_Address { get; set; } = string.Empty;
+        [Required]
+        [Column("phone")]
+        public string Phone { get; set; } = string.Empty;
+        [Required]
+        [Column("credentials")]
+        public string Credentials { get; set; } = string.Empty;
+    }
+}
